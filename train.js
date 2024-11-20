8bc168afd8cfe22332423edee0c1dcac8ddfd6da
Azamjon0898 <98kevin08@gmail.com>
2024-11-20 23:51:44 +0900

    21-dars vazifasi

--- train.js
+++ train.js
@@ -1,1 +1,34 @@
-// console.log('Train Area');
+console.log("Jack Ma maslahatlari:");
+const list = [
+    "Yaxshi talaba bo'ling", // 0-20
+    "To'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+    "O'zingizga ishlashingizni boshlang", // 30-40
+    "Siz kuchli bo'lgan narsalarni qiling", // 40-50
+    "Yoshlarga investitsiya qiling", // 50-60
+    "Endi dam oling, foydasi yo'q", // 60~
+    ""
+];
+
+function maslahatBering(a, callback) {
+    if(typeof a !== 'number') callback("insert a number", null);
+    else if(a <= 20) callback(null, list[0]);
+    else if(a > 20 && a <= 30) callback(null, list[1]);
+    else if(a > 30 && a <= 40) callback(null, list[2]);
+    else if(a > 40 && a <= 50) callback(null, list[3]);
+    else if(a > 50 && a <= 60) callback(null, list[4]);
+    else {
+        setTimeout(function () {
+            callback(null, list[5])
+        }, 5000);
+        
+    }
+}
+
+console.log('passed here 0');
+maslahatBering(65, (err, data) => {
+    if(err) console.log('ERROR:', err);
+    else {
+        console.log("javob", data);
+    }
+})
+console.log('passed here 1');
