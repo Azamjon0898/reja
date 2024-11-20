0e07f8bca9e18913b88dd5b9425917bbf4fa0fbf
Azamjon0898 <98kevin08@gmail.com>
2024-11-21 01:35:34 +0900

    22-dars

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const http = require("http");
 const fs = require("fs");
 
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+   if(err) {
+      console.log("ERROR:", err);
+   } else {
+      user = JSON.parse(data);
+   }
+});
 
 // 1 KIRISH code
 app.use(express.static("public"));
@@ -12,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // 2 Session code
-
 // 3 VIEWS code
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -24,7 +31,7 @@ app.post("/create-item", (req,res) => {
 });
 
 app.get('/author', (req, res) => {
-   res.render("author");
+   res.render("author", {user: user});
 });
 
 app.get("/", function (req, res) {
@@ -36,6 +43,6 @@ app.get("/", function (req, res) {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The server is running succesfully on the port: ${PORT}`);
+  console.log(`The server is running succesfully on port: ${PORT}`);
 });
 
