4a693e16d843ad88c8d0fd273e4a1cd9e47cf409
Azamjon0898 <98kevin08@gmail.com>
2024-11-25 00:04:23 +0900

    Fixed problems

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("web Serverni boshlash");
+const express = require("express");
+const app = express();
+// const fs = require("fs");
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//    if(err) {
+//       console.log("ERROR:", err);
+//    } else {
+//       user = JSON.parse(data);
+//    }
+// });
+
+//MongoDB connect
+
+// 1 KIRISH code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 Session code
+// 3 VIEWS code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 ROUTING code
+app.post("/create-item", (req,res) => {
+   // TODO: code with db here
+   // console.log(req.body);
+});
+
+// app.get('/author', (req, res) => {
+//    res.render("author", {user: user});
+// });
+
+app.get("/", function (req, res) {
+   res.render("reja");
+});
+
+module.exports = app;
+
+
--- server.js
+++ server.js
@@ -1,48 +1,11 @@
-console.log("web Serverni boshlash");
-const express = require('express');
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const app = require("./app");
 
-
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-   if(err) {
-      console.log("ERROR:", err);
-   } else {
-      user = JSON.parse(data);
-   }
-});
-
-// 1 KIRISH code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2 Session code
-// 3 VIEWS code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 ROUTING code
-app.post("/create-item", (req,res) => {
-   // TODO: code with db here
-   console.log(req.body);
-});
-
-app.get('/author', (req, res) => {
-   res.render("author", {user: user});
-});
-
-app.get("/", function (req, res) {
-   res.render("reja");
-});
-
-
+let db;
+const connectionString = ""
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
 });
-
